210fc4482bb34e515742ffdc68d67e8d0109c0e7
bugryder <bugryder@gmail.com>
2017-09-26 22:09:42 +0800

    add gitgnore & missing file

---
+++ NorthWind.Domain/PageSetting.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Domain
+{
+    public class PageSetting
+    {
+        /// <summary>
+        /// 頁數
+        /// </summary>
+        public int PageNumber
+        {
+            get { return _PageNumber; }
+            set { _PageNumber = value; }
+        }
+        /// <summary>
+        /// 每筆筆數
+        /// </summary>
+        public int PageSize
+        {
+            get { return _PageSize; }
+            set { _PageSize = value; }
+        }
+        /// <summary>
+        /// 排序的主鍵
+        /// </summary>
+        public String Key { get; set; }
+
+        public int PageCount { get; set; }
+        public int DataCount { get; set; }
+        public Boolean TurnOnPage
+        {
+            get { return _TurnOnPage; }
+            set { _TurnOnPage = value; }
+        }
+
+        private Boolean _TurnOnPage = false;
+        private int _PageNumber = 1;
+        private int _PageSize = 10;
+
+        public PageSetting()
+        {
+            this.PageNumber = 1;
+            this.PageSize = 10;
+        }
+
+        public PageSetting(int pageNumber, int pageSize)
+        {
+            SetParameter(pageNumber, pageSize, "");
+        }
+
+        public void SetParameter(int pageNumber, int pageSize, String key)
+        {
+            this.PageNumber = pageNumber;
+            this.PageSize = pageSize;
+            this.Key = key;
+
+        }
+
+
+        public void SetParameter(PageSetting inputPageSeting)
+        {
+            this.PageNumber = inputPageSeting.PageNumber;
+            this.PageSize = inputPageSeting.PageSize;
+            this.Key = inputPageSeting.Key;
+            TurnOnPage = true;
+        }
+    }
+}
---
+++ NorthWind.Infrastructure/Log.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Infrastructure
+{
+    public static class Log
+    {
+        private static string logWritePath = ConfigurationManager.AppSettings["LogWritePath"];
+        static string _fileName = "NorWind.API_" + DateTime.Now.ToString("yyyyMMdd") + ".txt";
+
+        public static void SqlError(Exception ex, string sql, string remark)
+        {
+            try
+            {
+                string dateNowStr = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+
+                string text = "";
+                text += "[SQL執行錯誤] " + dateNowStr + "\r\n";
+                text += "[SQL語法] \r\n";
+                text += sql + "\r\n";
+                text += "[Exception] \r\n";
+                text += ex.ToString() + "\r\n";
+                if (!string.IsNullOrEmpty(remark))
+                {
+                    text += "[備註] ";
+                    text += remark;
+                    text += "\r\n";
+
+                }
+                WriteLog(_fileName, text);
+            }
+            catch
+            { }
+
+        }
+
+
+
+        public static void WriteLog(string fileName, string text)
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter(logWritePath + _fileName, true);
+                sw.WriteLine(DateTime.Now.ToString("yyyyMMdd HH:mm") + ":" + text);
+                sw.Close();
+            }
+            catch
+            { }
+        }
+
+
+    }
+}
