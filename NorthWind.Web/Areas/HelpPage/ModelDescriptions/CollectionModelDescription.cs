a3548a43d4884e5811435213c5740a614b0d57ca
bugryder <bugryder@gmail.com>
2017-09-26 21:05:21 +0800

    delete useless project file

--- NorthWind.Web/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace NorthWind.Web.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- NorthWind.Web/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace NorthWind.Web.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
